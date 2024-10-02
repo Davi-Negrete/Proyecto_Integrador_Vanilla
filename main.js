6ef9bd62b4550a968b77ed22ce20369591faf26e
Davi-Negrete <luisdavidnegrete12@gmail.com>
2024-10-02 01:41:42 -0500

    Agrego todas las correcciones para terminar la etapa 2 del proyecto

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 import './sass/main.scss'
 import Handlebars from "handlebars"
 
+const calcularYearDinamico = () => {
 
-//! Creo dinamicamente con js el contenido de la etiqueta <small> dentro del footer, en este caso, toda la info de los CopyRight
+    //! Creo dinamicamente con js el contenido de la etiqueta <small> dentro del footer, en este caso, toda la info de los CopyRight
 
-//? Establezco la fecha del año en el footer <small> (CopyRight) de manera automática con el objeto Date.
-//? Para que no me toque hacerlo manualmente y cambiarla cada año.
+    //? Establezco la fecha del año en el footer <small> (CopyRight) de manera automática con el objeto Date.
+    //? Para que no me toque hacerlo manualmente y cambiarla cada año.
 
-// main.js
-const footerCopyRight = document.querySelector('.footer__seccion-2__small')
+    // main.js
+    const footerCopyRight = document.querySelector('.footer__seccion-2__small')
 
-const currentYear = new Date().getFullYear()
-const copyrightSymbol = '&copy;'
-const muebleriaText = 'LA MUEBLERÍA'
-const brandIcon = '<i class="fa-solid fa-couch"></i>'
-const derechosReservadosText = ' TODOS LOS DERECHOS RESERVADOS.'
+    const currentYear = new Date().getFullYear()
+    const copyrightSymbol = '&copy;'
+    const muebleriaText = 'LA MUEBLERÍA'
+    const brandIcon = '<i class="fa-solid fa-couch"></i>'
+    const derechosReservadosText = ' TODOS LOS DERECHOS RESERVADOS.'
 
-footerCopyRight.innerHTML = `${copyrightSymbol} ${currentYear} <b>${muebleriaText} ${brandIcon}</b> -${derechosReservadosText}`
+    footerCopyRight.innerHTML = `${copyrightSymbol} ${currentYear} <b>${muebleriaText} ${brandIcon}</b> -${derechosReservadosText}`
 
+}
  
 //? Hago las peticiones Asincronicas a habndlebars para obtener las plantillas de las card de los Productos.
 //! Queremos eliminar todo el codigo html de las card para generarlo dinamicamente con handlebars.
@@ -25,9 +27,12 @@ footerCopyRight.innerHTML = `${copyrightSymbol} ${currentYear} <b>${muebleriaTex
 
 const start = async () => {
 
-    const url = import.meta.env.VITE_BACKEND_PRODUCTOS
+    const endpoitLocal = import.meta.env.VITE_LOCAL
+
+    const endpoitRemoto = import.meta.env.VITE_REMOTO
     
     try {
+
         const respuesta = await fetch('templates/card.hbs')
 
         if ( !respuesta.ok) {
@@ -42,10 +47,15 @@ const start = async () => {
 
         // A la platilla que me genera, le voy a pasar la data Me genera un string.
 
-        // const respuestaBack = await fetch('http://localhost:8080/productos/') es local(del db-json), cuando lo suba a producccion(NETLIFY) no llega la data
-        const respuestaBack = await fetch(url) //? utilizo MOCKAPI (servidor externo.) para subir el proyecto a netlify y se pueda subir la data a produccion
+        let respuestaBack 
 
-        if ( !respuesta.ok) {
+        if ( window.location.href.includes('localhost') ) {
+            respuestaBack = await fetch (endpoitLocal)
+        } else {
+            respuestaBack = await fetch (endpoitRemoto)
+        }
+       
+        if ( !respuestaBack.ok) {
             throw new Error('Lo sentimos, algo ha pasado con los productos', respuestaBack.status)
         }
 
@@ -63,9 +73,23 @@ const start = async () => {
 
     } catch (error) {
         console.error(error)
-        
     }
 }
 
 
-window.addEventListener('DOMContentLoaded', start) // llamo a la funcion start, una vez se haya cargado completo el documento html
+window.addEventListener('DOMContentLoaded', () => {
+
+    calcularYearDinamico()
+
+    const ruta = window.location.href
+
+    if ( ruta.includes('contacto.html') ) {
+        console.log('Estoy en Contacto')
+    } else if ( ruta.includes('nosotros.html') ) {
+        console.log('Estoy en Nosotros')
+    } else {
+        start()
+    }
+}) 
+
+// llamo a la funcion start y YearDinamico, una vez se haya cargado completo el documento html
